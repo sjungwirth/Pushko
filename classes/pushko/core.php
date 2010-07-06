38e58a5f4103549d684276959fae6b8634eab742
2bj <gml2bj@gmail.com>
2010-07-06 23:31:51 +0400

    move settings in argument to config; add factory method;

--- classes/pushko/core.php
+++ classes/pushko/core.php
@@ -4,58 +4,89 @@
  * Pushko (Push[er]ko[hana])
  * This is a very simple PHP library for Kohana to the Pusher API
  *
- *    $pusher = new Pushko(APIKEY, SECRET, APP_ID, CHANNEL, [Debug: TRUE/FALSE, HOST, PORT]);
- *    $pusher->trigger('event_name', 'channel_name', [socket_id, Debug: TRUE/FALSE]);
+ *    $pusher = Pushko::factory(CHANNEL, [config_name]);
+ *    $pusher->trigger('event_name', 'data', [$socket_id = NULL, $channel = '', $debug = FALSE]);
  *    $pusher->socket_auth('socket_id');
  *
- * @author		2bj
- * @author	    Squeeks
- * @author		Paul44 (http://github.com/Paul44)
- * @author		Ben Pickles (http://github.com/benpickles)
- * @author		Mastercoding (http://www.mastercoding.nl)
- * @copyright  (c) 2010 2bj
- * @copyright  (c) 2010 Squeeks
- * @license     http://www.opensource.org/licenses/mit-license.php
+ * @author	2bj
+ * @author	Squeeks
+ * @author	Paul44 (http://github.com/Paul44)
+ * @author	Ben Pickles (http://github.com/benpickles)
+ * @author	Mastercoding (http://www.mastercoding.nl)
+ * @copyright	(c) 2010 2bj
+ * @copyright	(c) 2010 Squeeks
+ * @license	http://www.opensource.org/licenses/mit-license.php
  */
 abstract class Pushko_Core {
+	
+	const API_AUTH_VERSION = '1.0';
 
 	/**
-	 * @var	array Settings
+	 * @var	string	Default config name
 	 */
-	private $settings = array ();
+	public static $default_config_name = 'default';
 
 	/**
-	 * Create a new Pushko object.
-	 *
-	 * Initializes a new Pushko instance with key, secret , app ID and channel.
-	 * You can optionally turn on debugging for all requests by setting debug to true.
-	 *
-	 * @param	string	$auth_key
-	 * @param	string	$secret
-	 * @param	integer	$app_id
-	 * @param	string	$channel
-	 * @param	boolean	$debug
-	 * @param	string	$host
-	 * @param	integer	$port
-	 * @param	integer	$timeout
+	 * @var	array	Configuration array
 	 */
-	public function __construct(
-			$auth_key, $secret, $app_id, $channel = '',
-			$debug = FALSE, $host = 'http://api.pusherapp.com',
-			$port = '80', $timeout = 30 )
+	protected $_config;
+	
+	/**
+	 * Create and return new Pusko object
+	 * 
+	 * @param string $channel
+	 * @param string $name
+	 * @return Pushko
+	 */
+	public static function factory($channel, $name = NULL)
 	{
+		return new Pushko($channel, $name);
+	}
 
-		// Setup defaults
-		$this->settings['server']	= $host;
-		$this->settings['port']		= $port;
-		$this->settings['auth_key']	= $auth_key;
-		$this->settings['secret']	= $secret;
-		$this->settings['app_id']	= $app_id;
-		$this->settings['channel']	= $channel;
-		$this->settings['url']		= '/apps/' . $this->settings['app_id'];
-		$this->settings['debug']	= $debug;
-		$this->settings['timeout']	= $timeout;
+	/**
+	 * Init Pushko
+	 *
+	 * @param string $channel
+	 * @param string $name
+	 */
+	public function __construct($channel, $name = NULL)
+	{
+		if ($name === NULL)
+		{
+			$name = self::$default_config_name;
+		}
 
+		$this->_config = Kohana::config('pushko')->$name;
+
+		if ( ! isset($this->_config['app_id'])		OR
+			   $this->_config['app_id'] === NULL	OR
+			   $this->_config['app_id'] == ''
+			)
+		{
+			throw new Pushko_Exception('Pusher app_id is not defined in :name configuration',
+					array(':name' => $name));
+		}
+
+		if ( ! isset($this->_config['auth_key'])	OR
+			   $this->_config['auth_key'] === NULL	OR
+			   $this->_config['auth_key'] == ''
+			)
+		{
+			throw new Pushko_Exception('Pusher auth_key is not defined in :name configuration',
+					array(':name' => $name));
+		}
+
+		if ( ! isset($this->_config['secret'])		OR
+			   $this->_config['secret'] === NULL	OR
+			   $this->_config['secret'] == ''
+			)
+		{
+			throw new Pushko_Exception('Pusher secret is not defined in :name configuration',
+					array(':name' => $name));
+		}
+
+		$this->_config['channel'] = $channel;
+		$this->_config['url'] = '/apps/' . $this->_config['app_id'];
 	}
 
 	/**
@@ -69,7 +100,7 @@ abstract class Pushko_Core {
 	 * @param	boolean	$debug [optional]
 	 * @return	mixed
 	 */
-	public function trigger( $event, $payload, $socket_id = NULL, $channel = '', $debug = FALSE )
+	public function trigger($event, $payload, $socket_id = NULL, $channel = '', $debug = FALSE)
 	{
 
 		// Check if we can initialize a cURL connection
@@ -80,12 +111,16 @@ abstract class Pushko_Core {
 		}
 
 		// Add channel to URL..
-		$s_url = $this->settings['url'].'/channels/'.($channel != '' ? $channel : $this->settings['channel']).'/events';
+		$s_url = $this->_config['url'].'/channels/'
+				 .($channel != '' ? $channel : $this->_config['channel'])
+				 .'/events';
 
 		// Build the request
 		$signature = "POST\n".$s_url."\n";
 		$payload_encoded = json_encode($payload);
-		$query = "auth_key=".$this->settings['auth_key']."&auth_timestamp=".time()."&auth_version=1.0&body_md5=".md5($payload_encoded)."&name=".$event;
+		$query = "auth_key=".$this->_config['auth_key']."&auth_timestamp="
+			     .time()."&auth_version=".self::API_AUTH_VERSION."&body_md5="
+				 .md5($payload_encoded)."&name=".$event;
 
 		// Socket ID set?
 		if ($socket_id !== NULL )
@@ -94,9 +129,10 @@ abstract class Pushko_Core {
 		}
 
 		// Create the signed signature...
-		$auth_signature = hash_hmac('sha256', $signature . $query, $this->settings['secret'], FALSE);
+		$auth_signature = hash_hmac('sha256', $signature.$query, $this->_config['secret'], FALSE);
 		$signed_query = $query."&auth_signature=".$auth_signature;
-		$full_url = $this->settings['server'].':'.$this->settings['port'].$s_url.'?'.$signed_query;
+		$full_url = $this->_config['server'].':'.$this->_config['port']
+				    .$s_url.'?'.$signed_query;
 
 		// Set cURL opts and execute request
 		curl_setopt($ch, CURLOPT_URL, $full_url);
@@ -104,7 +140,7 @@ abstract class Pushko_Core {
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_POST, 1);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $payload_encoded);
-		curl_setopt($ch, CURLOPT_TIMEOUT, $this->settings['timeout']);
+		curl_setopt($ch, CURLOPT_TIMEOUT, $this->_config['timeout']);
 
 		$response = curl_exec($ch);
 
@@ -114,7 +150,7 @@ abstract class Pushko_Core {
 		{
 			return TRUE;
 		}
-		elseif ($debug === TRUE || $this->settings['debug'] === TRUE)
+		elseif ($debug === TRUE || $this->_config['debug'] === TRUE)
 		{
 			return $response;
 		}
@@ -134,11 +170,11 @@ abstract class Pushko_Core {
 	public function socket_auth($socket_id)
 	{
 		$signature = hash_hmac(
-			'sha256', $socket_id.':'.$this->settings['channel'],
-			$this->settings['secret'],
+			'sha256', $socket_id.':'.$this->_config['channel'],
+			$this->_config['secret'],
 			FALSE
 		);
-		$signature = array('auth' => $this->settings['auth_key'].':'.$signature);
+		$signature = array('auth' => $this->_config['auth_key'].':'.$signature);
 
 		return json_encode($signature);
 	}
---
+++ config/pushko.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'default' => array(
+		'app_id' => NULL,
+		'auth_key' => NULL,
+		'secret' => NULL,
+		'server' => 'http://api.pusherapp.com',
+		'port' => '80',
+		'timeout' => '30',
+		'debug' => FALSE,
+	)
+);
