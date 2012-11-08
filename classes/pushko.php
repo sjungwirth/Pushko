52b3c07b3c316ef522fd30d18beb3f466f93f9ca
Scott Jungwirth <scott.jungwirth@demandmedia.com>
2012-11-08 11:28:26 -0800

    MAJOR: updated to use new API format possible to broadcast to multiple channels at once

--- classes/pushko.php
+++ classes/pushko.php
@@ -1,24 +1,3 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Pushko extends Pushko_Core {
-
-	/**
-	* Creates a presence signature (an extension of socket signing)
-	*
-	* @param int $socket_id
-	* @param string $user_id
-	* @param mixed $user_info
-	* @return string
-	*/
-	public function presence_auth($socket_id, $user_id, $user_info = null )
-	{
-
-		$user_data = array( 'user_id' => $user_id );
-		if($user_info)
-		{
-			$user_data['user_info'] = $user_info;
-		}
-
-		return $this->socket_auth($socket_id, $user_data);
-	}
-}
+class Pushko extends Pushko_Core {}
--- classes/pushko/core.php
+++ classes/pushko/core.php
@@ -27,29 +27,27 @@ abstract class Pushko_Core {
 	public static $default_config_name = 'default';
 
 	/**
-	 * @var	array	Configuration array
-	 */
-	protected $_config;
-	
-	/**
 	 * Create and return new Pusko object
-	 * 
-	 * @param string $channel
+	 *
 	 * @param string $name
 	 * @return Pushko
 	 */
-	public static function factory($channel, $name = NULL)
+	public static function factory($name = NULL)
 	{
-		return new Pushko($channel, $name);
+		return new Pushko($name);
 	}
+
+	/**
+	 * @var	array	Configuration array
+	 */
+	protected $_config;
 
 	/**
 	 * Init Pushko
 	 *
-	 * @param string $channel
 	 * @param string $name
 	 */
-	public function __construct($channel, $name = NULL)
+	public function __construct($name = NULL)
 	{
 		if ($name === NULL)
 		{
@@ -58,51 +56,41 @@ abstract class Pushko_Core {
 
 		$this->_config = Kohana::$config->load('pusher')->$name;
 
-		if ( ! isset($this->_config['app_id'])		OR
-			   $this->_config['app_id'] === NULL	OR
-			   $this->_config['app_id'] == ''
-			)
+		if (empty($this->_config['app_id']))
 		{
 			throw new Pushko_Exception('Pusher app_id is not defined in :name configuration',
 					array(':name' => $name));
 		}
 
-		if ( ! isset($this->_config['auth_key'])	OR
-			   $this->_config['auth_key'] === NULL	OR
-			   $this->_config['auth_key'] == ''
-			)
+		if (empty($this->_config['auth_key']))
 		{
 			throw new Pushko_Exception('Pusher auth_key is not defined in :name configuration',
 					array(':name' => $name));
 		}
 
-		if ( ! isset($this->_config['secret'])		OR
-			   $this->_config['secret'] === NULL	OR
-			   $this->_config['secret'] == ''
-			)
+		if (empty($this->_config['secret']))
 		{
 			throw new Pushko_Exception('Pusher secret is not defined in :name configuration',
 					array(':name' => $name));
 		}
 
-		$this->_config['channel'] = $channel;
-		$this->_config['url'] = '/apps/' . $this->_config['app_id'];
+		$this->_config['api'] = '/apps/'.$this->_config['app_id'];
 	}
 
 	/**
 	 * Trigger an event by providing event name and payload.
 	 * Optionally provide a socket ID to exclude a client (most likely the sender).
+	 * @throws Pushko_Exception
 	 *
+	 * @param	mixed	$channels
 	 * @param	string	$event
-	 * @param	mixed	$payload
+	 * @param	mixed	$data
 	 * @param	integer	$socket_id [optional]
-	 * @param	string	$channel [optional]
 	 * @param	boolean	$debug [optional]
 	 * @return	mixed
 	 */
-	public function trigger($event, $payload, $socket_id = NULL, $channel = '', $debug = FALSE)
+	public function trigger($channels, $event, $data, $socket_id = NULL, $debug = FALSE)
 	{
-
 		// Check if we can initialize a cURL connection
 		$ch = curl_init();
 		if ($ch === FALSE)
@@ -110,32 +98,32 @@ abstract class Pushko_Core {
 			throw new Pushko_Exception('Could not initialise cURL!');
 		}
 
-		// Add channel to URL..
-		$s_url = $this->_config['url'].'/channels/'
-				 .($channel != '' ? $channel : $this->_config['channel'])
-				 .'/events';
+		$path = $this->_config['api'].'/events';
 
 		// Build the request
-		$signature = "POST\n".$s_url."\n";
-		$payload_encoded = json_encode($payload);
-		$query = "auth_key=".$this->_config['auth_key']."&auth_timestamp="
-			     .time()."&auth_version=".self::API_AUTH_VERSION."&body_md5="
-				 .md5($payload_encoded)."&name=".$event;
+		$request_params = array(
+			'data' => json_encode($data),
+			'name' => $event,
+			'channels' => (array) $channels,
+		);
 
-		// Socket ID set?
-		if ($socket_id !== NULL )
+		// socketid
+		if ($socket_id !== NULL)
 		{
-			$query .= '&socket_id='.$socket_id;
+			$request_params['socket_id'] = $socket_id;
 		}
 
-		// Create the signed signature...
-		$auth_signature = hash_hmac('sha256', $signature.$query, $this->_config['secret'], FALSE);
-		$signed_query = $query."&auth_signature=".$auth_signature;
-		$full_url = $this->_config['server'].':'.$this->_config['port']
-				    .$s_url.'?'.$signed_query;
+		$query_params = array();
+		$payload_encoded = json_encode($request_params);
+		$query_params['body_md5'] = md5($payload_encoded);
+
+		// Create the signed query...
+		$signed_query = $this->auth_signature('POST', $path, $query_params);
+
+		$url = $this->_config['server'].':'.$this->_config['port'].$path.'?'.$signed_query;
 
 		// Set cURL opts and execute request
-		curl_setopt($ch, CURLOPT_URL, $full_url);
+		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_POST, 1);
@@ -146,7 +134,7 @@ abstract class Pushko_Core {
 
 		curl_close($ch);
 
-		if ($response == "202 ACCEPTED\n" && $debug == FALSE)
+		if ($response == "{}" && $debug == FALSE)
 		{
 			return TRUE;
 		}
@@ -158,26 +146,106 @@ abstract class Pushko_Core {
 		{
 			return FALSE;
 		}
+	}
 
+	public function get_channels()
+	{
+		// Check if we can initialize a cURL connection
+		$ch = curl_init();
+		if ($ch === FALSE)
+		{
+			throw new Pushko_Exception('Could not initialise cURL!');
+		}
+
+		$path = $this->_config['api'].'/channels';
+
+		// Create the signed signature...
+		$signed_query = $this->auth_signature('GET', $path);
+
+		$url = $this->_config['server'].':'.$this->_config['port'].$path.'?'.$signed_query;
+
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_TIMEOUT, $this->_config['timeout']);
+
+		$response = curl_exec($ch);
+
+		$http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+		if ($http_status == 200)
+		{
+			$response = json_decode($response);
+			$response = $response->channels;
+		}
+		else
+		{
+			$response = false;
+		}
+
+		curl_close($ch);
+
+		return $response;
+	}
+
+	public function get_channel_stats($channel)
+	{
+		// Check if we can initialize a cURL connection
+		$ch = curl_init();
+		if ($ch === FALSE)
+		{
+			throw new Pushko_Exception('Could not initialise cURL!');
+		}
+
+		$path = $this->_config['api'].'/channels/'.$channel.'/stats';
+
+		// Create the signed signature...
+		$signed_query = $this->auth_signature('GET', $path);
+
+		$url = $this->_config['server'].':'.$this->_config['port'].$path.'?'.$signed_query;
+
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_TIMEOUT, $this->_config['timeout']);
+
+		$response = curl_exec($ch);
+
+		$http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+		if ($http_status == 200)
+		{
+			$response = json_decode($response);
+		}
+		else
+		{
+			$response = false;
+		}
+
+		curl_close($ch);
+
+		return $response;
 	}
 
 	/**
 	 * Creates a socket signature
 	 *
+	 * @param	string	$channel
 	 * @param	integer	$socket_id
 	 * @param	mixed	$data
 	 * @return	array
 	 */
-	public function socket_auth($socket_id, $data = null)
+	public function socket_auth($channel, $socket_id, $data = null)
 	{
 		if ($data)
 		{
+			// encode the data
 			$data = (is_string($data) ? $data : json_encode($data));
 		}
 
-		$signature = array(
+		$auth = array(
 			'auth' => $this->_config['auth_key'].':'.hash_hmac(
-				'sha256', $socket_id.':'.$this->_config['channel'].($data?':'.$data:null),
+				'sha256', $socket_id.':'.$channel.($data?':'.$data:null),
 				$this->_config['secret'],
 				FALSE
 			),
@@ -186,9 +254,63 @@ abstract class Pushko_Core {
 		// add the custom data if it has been supplied
 		if ($data)
 		{
-			$signature['channel_data'] = $data;
+			$auth['channel_data'] = $data;
 		}
 
-		return $signature;
+		return $auth;
+	}
+
+	/**
+	* Creates a presence signature (an extension of socket signing)
+	*
+	* @param int $channel
+	* @param int $socket_id
+	* @param string $user_id
+	* @param mixed $user_info
+	* @return array
+	*/
+	public function presence_auth($channel, $socket_id, $user_id, $user_info = null )
+	{
+
+		$user_data = array('user_id' => $user_id);
+		if($user_info)
+		{
+			$user_data['user_info'] = $user_info;
+		}
+
+		return $this->socket_auth($channel, $socket_id, $user_data);
+	}
+
+	/**
+	 * @param $request_method
+	 * @param $request_path
+	 * @param array $query_params
+	 * @param string $auth_version
+	 * @param null $auth_timestamp
+	 * @return string
+	 */
+	public function auth_signature($request_method, $request_path,
+	  $query_params = array(), $auth_version = '1.0', $auth_timestamp = null)
+	{
+		$auth_key = $this->_config['auth_key'];
+		$auth_secret = $this->_config['secret'];
+		$params = array();
+		$params['auth_key'] = $auth_key;
+		$params['auth_timestamp'] = (is_null($auth_timestamp)?time() : $auth_timestamp);
+		$params['auth_version'] = $auth_version;
+
+		$params = array_merge($params, $query_params);
+		ksort($params);
+
+		$string_to_sign = "$request_method\n".$request_path."\n".implode('&', Arr::kv_pair($params));
+
+		$auth_signature = hash_hmac( 'sha256', $string_to_sign, $auth_secret, false );
+
+		$params['auth_signature'] = $auth_signature;
+		ksort($params);
+
+		$auth_query_string = implode('&', Arr::kv_pair($params));
+
+		return $auth_query_string;
 	}
 }
